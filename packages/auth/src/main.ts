364c830e69514289e1eb8bdbec3df786205bb9c8
dmitry.yegorov <dmitry.yegorov@effective-soft.com>
2022-10-22 19:01:05 +0300

    formated code via prettier

--- packages/auth/src/main.ts
+++ packages/auth/src/main.ts
@@ -10,8 +10,9 @@ async function bootstrap() {
       transport: Transport.KAFKA,
       options: {
         client: {
-        brokers: ['localhost:9092'],
-      },}
+          brokers: ['localhost:9092'],
+        },
+      },
     },
   );
   await app.listen();
